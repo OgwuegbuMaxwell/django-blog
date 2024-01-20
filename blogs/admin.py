e683441437ac666ce3e5bf11aa8beafbe852a239
Ogwuegbu Maxwell <53094485+OgwuegbuMaxwell@users.noreply.github.com>
2024-01-20 12:59:42 -0500

    Created blog app and the models needed

---
+++ blogs/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Category
+from .models import Blog
+# Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    # Handle populating slug dynamically
+    # As we write the blog title, the slug should be populated
+    prepopulated_fields = {'slug': ('title',)}
+    # List all the fields you want to be shown on the admin
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    # Add search functionality
+    # to search foreign key like category, we use category__category_name
+    # it means we are not searching directly the category filed in the Blog model,
+    # we are going to the category model, and seach the category_name field
+    # so we are pointing to Category Model >> category_name field.
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+    # If you want to be able to edit a particular field directly on the listed rows
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
---
+++ blogs/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+# Import User
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # To tell django the correct plural spelling of Category
+    # If not django will write it as categorys in the admin panel
+    class Meta:
+       verbose_name_plural =  'categories'
+    
+    # Whenever you write any member function inside the class,
+    # you should always pass self as a parameter.
+    # Here, self is nothing but the Category model itself.
+    # This is setting the string representation of this particular model
+    def __str__(self):
+        return self.category_name
+
+
+
+# Making drop down
+STATUS_CHOICES =(
+    ("Draft", "Draft"),
+    ("Published", "Published")
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    # A slog is the part of a URL that identifies a particular page on a website
+    # In fact, It's a title itself with the hyphen instead of the space in between words
+    # The slug represent the title, and it's a part of the URL part, seperated with hyphens
+    # Slug is very uuseful when it comes to SEO
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    # When you delete a specific category, then the blog post associated with that
+    # particular category should also be deleted.
+    # That means we will delete all those posts of the category associated with it is deleted
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # If the user is deleted, this particular blog post should be deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # This image will be uploaded to:
+    # uploads/the year the blog was created/the month/ the day
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=1000)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Draft")
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # Remember to install pillow since we are working with image
+    # pip install Pillow
+    # Make sure the virtual environment is activated when installing the pillow
+    
+
+    # Whenever you write any member function inside the class,
+    # you should always pass self as a parameter.
+    # Here, self is nothing but the Category model itself.
+    # This is setting the string representation of this particular model
+    def __str__(self):
+        return self.title
+    
+    # Don't forget to make migration
+    # python manage.py makemigrations
+    # python manage.py migrate
+    
+    # also don't forget to register the model to the admin.py
+    # admin.site.register(Blog)
+    
+
+
